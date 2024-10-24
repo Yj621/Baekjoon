34232a99d0ef31fbdcd766d3f20289f509141590
Yj621 <87031282+Yj621@users.noreply.github.com>
2024-10-24 18:40:35 +0900

    10757

---
+++ Baekjoon_10757/Baekjoon_10757/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Baekjoon_10757
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            BigInteger A = BigInteger.Parse(input[0]);
+            BigInteger B = BigInteger.Parse(input[1]);  
+
+            Console.WriteLine(A + B);
+        }
+    }
+}
