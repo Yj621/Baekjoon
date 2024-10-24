25b9d9a6946ab9100b3de45e2056f9d76b95648a
Yj621 <87031282+Yj621@users.noreply.github.com>
2024-10-24 10:14:42 +0900

    10828

---
+++ Baekjoon_10828/Baekjoon_10828/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace Baekjoon_10828
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            Stack<int> stack = new Stack<int>();
+
+            int N = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < N; i++) 
+            {
+                string[] input = Console.ReadLine().Split();
+                string fun =input[0];
+
+                switch(fun)
+                {
+                    case "push":
+                        int value = int.Parse(input[1]); 
+                        stack.Push(value);
+                        break;
+
+                    case "pop":
+                        if(stack.Count == 0)
+                        {
+                            sb.AppendLine("-1");
+                        }
+                        else
+                        {
+                            sb.AppendLine(stack.Pop().ToString());
+                        }
+                        break;
+
+                    case "size":
+                        sb.AppendLine(stack.Count().ToString());
+                        break;
+
+                    case "empty":
+                        if(stack.Count() == 0)
+                        {
+                            sb.AppendLine("1");
+                        }
+                        else
+                        {
+                            sb.AppendLine("0");
+                        }
+                        break;
+
+                    case "top":
+                        if(stack.Count ==0)
+                        {
+                            sb.AppendLine("-1");
+                        }
+                        else
+                        {
+                            sb.AppendLine(stack.Peek().ToString());
+                        }
+                        break;
+
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
