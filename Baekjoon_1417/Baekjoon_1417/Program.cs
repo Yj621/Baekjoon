1ee19627ee99aceb4fc10709abab7c54d0352264
Yj621 <87031282+Yj621@users.noreply.github.com>
2024-10-24 16:42:31 +0900

    1417 & 18258 & 10828

---
+++ Baekjoon_1417/Baekjoon_1417/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace Baekjoon_1417
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = int.Parse(sr.ReadLine());
+            int dasom = int.Parse(sr.ReadLine());
+
+            int[] candidate = new int[N];
+            int count = 0;
+
+            bool isBig = false;
+
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                candidate[i] = int.Parse(sr.ReadLine());
+            }
+
+            while (!isBig)
+            {
+                isBig = true;
+                Array.Sort(candidate);
+                Array.Reverse(candidate);
+                if (dasom <= candidate[0])
+                {
+                    isBig = false;
+                    candidate[0]--;
+                    dasom++;
+                    count++;
+                }
+            }
+            sw.WriteLine(count);
+
+            sw.Close();
+            sr.Close();
+        }
+
+    }
+}
---
+++ Baekjoon_18258/Baekjoon_18258/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.IO;
+
+namespace Baekjoon_18258
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            Queue<int> queue = new Queue<int>();
+            int back = 0;
+            int N = int.Parse(sr.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] input = sr.ReadLine().Split();
+                string fun = input[0];
+                switch (fun)
+                {
+                    case "push":
+                        int value = int.Parse(input[1]);
+                        back = value;
+                        queue.Enqueue(value);
+                        break;
+
+                    case "pop":
+                        if (queue.Count == 0)
+                        {
+                            sw.WriteLine("-1");
+                        }
+                        else
+                        {
+                            sw.WriteLine(queue.Dequeue().ToString());
+
+                        }
+                        break;
+
+                    case "empty":
+                        if (queue.Count == 0)
+                        {
+                            sw.WriteLine("1");
+                        }
+                        else
+                        {
+                            sw.WriteLine("0");
+                        }
+                        break;
+
+                    case "size":
+                        sw.WriteLine(queue.Count().ToString());
+                        
+                        break;
+
+                    case "front":
+                        if (queue.Count == 0)
+                        {
+                            sw.WriteLine("-1");
+                        }
+                        else
+                        {
+                            sw.WriteLine(queue.Peek().ToString());
+                        }
+                        break;
+
+                    case "back":
+                        if (queue.Count == 0)
+                        {
+                            sw.WriteLine("-1");
+                        }
+                        else
+                        {
+                            sw.WriteLine(back.ToString());
+                        }
+
+                        break;
+                }
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
