abaf43b96c44c701e4e929a73699e918e66008e5
Yj621 <wjddbswl26@gmail.com>
2025-05-10 02:04:01 +0900

    1463

--- Baekjoon_1463/Baekjoon_1463/Program.cs
+++ Baekjoon_1463/Baekjoon_1463/Program.cs
@@ -1,35 +1,33 @@
-﻿using System.Runtime.Intrinsics.Arm;
-
-namespace Baekjoon
+﻿namespace Baekjoon
 {
     class Program
     {
         static void Main(string[] args)
         {
             int N = int.Parse(Console.ReadLine());
+            
+            // 1로 만드는 최소 연산 횟수
+            int[] dp = new int[N + 1];
 
-            int[] dp = new int[N + 1]; //1로 가는 최소 연산 횟수
-
-            dp[1] = 0; //1로 가는 건 0임
-
-            for (int i = 2; i <= N; i++)
+            for(int i = 2; i<=N;i++)
             {
+                //1빼기 했을때 최소 횟수 +1
                 dp[i] = dp[i - 1] + 1;
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine("dp1 : " + dp[i]);
-                    dp[i] = Math.Min(dp[i], dp[i / 2] + 1);
-                    Console.WriteLine("dp2 : " + dp[i]);
-                }
+
+                //3으로 나눌때 최소 횟수 +1
                 if (i % 3 == 0)
                 {
                     dp[i] = Math.Min(dp[i], dp[i / 3] + 1);
                 }
-            }
 
+                //2으로 나눌때 최소 횟수 +1
+                if (i % 2 == 0)
+                {
+                    dp[i] = Math.Min(dp[i], dp[i / 2] + 1);
+                }
+            }
             Console.WriteLine(dp[N]);
         }
-
 
     }
 }
