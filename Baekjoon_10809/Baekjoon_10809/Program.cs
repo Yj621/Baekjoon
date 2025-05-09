55ab67785ffe2c10cf41578826f1f03f174ef2c3
Yj621 <wjddbswl26@gmail.com>
2025-05-10 00:26:27 +0900

    10809

---
+++ Baekjoon_10809/Baekjoon_10809/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Baekjoon_10809
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < 26; i++)
+            {
+                Console.Write(input.IndexOf((char)(97 + i)));
+
+                Console.Write(' ');
+            }
+        }
+    }
+}
