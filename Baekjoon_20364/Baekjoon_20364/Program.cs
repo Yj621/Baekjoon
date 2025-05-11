274104c998af0e2b59b9f2da7599dce19cf862e0
Yj621 <wjddbswl26@gmail.com>
2025-05-12 02:54:11 +0900

    20364

---
+++ Baekjoon_20364/Baekjoon_20364/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Baekjoon_20364
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            string[] input = sr.ReadLine().Split();
+
+            int N = int.Parse(input[0]);
+            int Q = int.Parse(input[1]);
+            bool[] occupy = new bool[N + 1];
+
+            while (Q > 0)
+            {
+                Q -= 1;
+
+                int x = int.Parse(sr.ReadLine());
+                int blocked = 0;
+
+                for (int i = x; i > 0; i /= 2)
+                {
+                    if (occupy[i])
+                    {
+                        blocked = i;
+                    }
+                }
+
+                sw.WriteLine(blocked);
+                if (blocked == 0)
+                {
+                    occupy[x] = true;
+                }
+            }
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
---
+++ Baekjoon_5622/Baekjoon_5622/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Baekjoon_5622
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(Dial(input));
+        }
+
+        static int Dial(string input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch(input[i])
+                {
+                    case <= 'C':
+                        sum += 3;
+                        break;
+                    case <= 'F':
+                        sum += 4;
+                        break;
+                    case <= 'I':
+                        sum += 5;
+                        break;
+                    case <= 'L':
+                        sum += 6;
+                        break;
+                    case <= 'O':
+                        sum += 7;
+                        break;
+                    case <= 'S':
+                        sum += 8;
+                        break;
+                    case <= 'V':
+                        sum += 9;
+                        break;
+                    case <= 'Z':
+                        sum += 10;
+                        break;
+                }
+            }
+            return sum;
+        }
+    }
+}
