1e3a3389c0404a4e1893f06edb176d4ab69577ac
Yj621 <wjddbswl26@gmail.com>
2025-05-14 19:31:04 +0900

    1620

---
+++ Baekjoon_1620/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+        int N = int.Parse(input[0]);    
+        int M = int.Parse(input[1]);    
+        var Pocketmon = new Dictionary<string, int>(N);
+        var names = new string[N+1];
+
+        for(int i = 1; i<=N; i++)
+        {
+            string name = Console.ReadLine().Trim();
+            Pocketmon[name] = i;
+            names[i] = name;
+        }
+        
+        var sb = new System.Text.StringBuilder();
+        
+        for (int i = 0; i < M; i++)
+        {
+            string m = Console.ReadLine().Trim();
+
+            if (int.TryParse(m, out int idx))
+            {
+                sb.AppendLine(names[idx]);
+            }
+            else
+            {
+                sb.AppendLine(Pocketmon[m].ToString());
+            }
+        }
+
+        Console.Write(sb);
+
+    }
+}
