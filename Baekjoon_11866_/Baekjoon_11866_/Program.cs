50915c2cdc6934aec89af284f9ea70b8ff1e99a8
Yj621 <wjddbswl26@gmail.com>
2025-05-18 22:30:09 +0900

    11866

---
+++ Baekjoon_11866_/Baekjoon_11866_/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Baekjoon_11866_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            Queue<int> queue = new Queue<int>();
+            Queue<int> result = new Queue<int>();
+            string[] input = sr.ReadLine().Split();
+
+            int N = int.Parse(input[0]);
+            int K = int.Parse(input[1]);
+
+            for (int i = 1; i <= N; i++)
+                queue.Enqueue(i);
+
+            while (queue.Count > 0)
+            {
+                for (int i = 1; i < K; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+                result.Enqueue(queue.Dequeue());
+            }
+
+            var sb = new StringBuilder();
+
+            sb.Append("<");
+            sb.Append(string.Join(", ",result));
+            sb.Append(">");
+            sw.Write(sb.ToString());
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
