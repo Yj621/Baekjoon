f235ce453f6ac6687d34a39aa8935a179728a87b
Yj621 <wjddbswl26@gmail.com>
2025-08-28 14:22:19 +0900

    10798 & 2566

---
+++ Baekjoon_10798/Baekjoon_10798/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Baekjoon_10798
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[5];
+            int maxLength = 0;
+            for (int i = 0; i<5; i++)
+            {
+                array[i] = Console.ReadLine();
+                if (array[i].Length > maxLength)
+                {
+                    maxLength = array[i].Length;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < maxLength; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i < array[j].Length)
+                    {
+                        sb.Append(array[j][i]);
+                    }
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ Baekjoon_2566/Baekjoon_2566/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Baekjoon_2566
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[9, 9];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = int.Parse(input[j]);
+                }
+            }
+
+            int maxV = 0;
+            int maxRow = 0;
+            int maxCol = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if(array[i, j] > maxV)
+                    {
+                        maxV = array[i, j];
+                        maxRow = i;
+                        maxCol = j;
+                    }
+                }
+            }
+            Console.WriteLine(maxV);
+            Console.WriteLine($"{maxRow+1} {maxCol+1}");
+        }
+    }
+}
