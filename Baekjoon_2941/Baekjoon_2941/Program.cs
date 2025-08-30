7e6556fdc473b3b65de6e9f55f88e11fb5930b7c
Yj621 <wjddbswl26@gmail.com>
2025-08-30 14:49:02 +0900

    2941

---
+++ Baekjoon_2941/Baekjoon_2941/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Baekjoon_2941
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alphet = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+            var word = Console.ReadLine();
+
+            foreach (string s in alphet)
+            {
+                word = word.Replace(s, "*");
+            }
+            Console.WriteLine(word.Length);
+        }
+    }
+}
