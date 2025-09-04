d9289b5595a58666656d482d52010c02095c0288
Yj621 <wjddbswl26@gmail.com>
2025-09-04 14:26:23 +0900

    1316

--- Baekjoon_1316/Baekjoon_1316/Program.cs
+++ Baekjoon_1316/Baekjoon_1316/Program.cs
@@ -4,13 +4,41 @@
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
+            int T = int.Parse(Console.ReadLine());
+            int groupWordCount = 0;
 
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < T; i++)
             {
-                string input = Console.ReadLine();
+                string word = Console.ReadLine();
+                if(IsGroupWord(word))
+                {
+                    groupWordCount++;
+                }
             }
+            Console.WriteLine(groupWordCount);
         }
-        
+
+        static bool IsGroupWord(string word)
+        {
+            List<char> appearChars = new List<char>();
+            if (word.Length > 0)
+            {
+                appearChars.Add(word[0]);
+            }
+
+            for(int i = 1; i<word.Length; i++)
+            {
+                if (word[i] != word[i-1])
+                {
+                    if(appearChars.Contains(word[i]))
+                    {
+                        return false;
+                    }
+                    appearChars.Add(word[i]);
+                }
+            }
+
+            return true;
+        }
     }
 }
