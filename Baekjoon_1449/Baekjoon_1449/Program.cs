58ced10cad88eabd6ef174251a7ccef85a2f6e1d
Yj621 <wjddbswl26@gmail.com>
2026-02-17 00:10:54 +0900

    1449

---
+++ Baekjoon_1449/Baekjoon_1449/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Baekjoon_1449
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int N = int.Parse(input[0]);
+            int L = int.Parse(input[1]);
+
+            int[] pos = new int[N];
+            string[] posInput = Console.ReadLine().Split();
+            for (int i = 0; i < N; i++)
+            {
+                pos[i] = int.Parse(posInput[i]);
+            }
+            Array.Sort(pos);
+
+            int count = 0;
+            int tapeEnd = 0;
+            foreach (int p in pos)
+            {
+                if (p > tapeEnd)
+                {
+                    count++;
+                    tapeEnd = p + L - 1;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
