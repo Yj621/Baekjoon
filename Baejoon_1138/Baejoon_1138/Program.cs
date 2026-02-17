82f8860dc7fe84e1b657b927781652fdb793869d
Yj621 <wjddbswl26@gmail.com>
2026-02-17 16:06:26 +0900

    1138

---
+++ Baejoon_1138/Baejoon_1138/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Baejoon_1138
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            int[] order = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                order[i] = int.Parse(input[i]);
+            }
+            List<int> result = new List<int>();
+
+            //큰 사람부터 역순으로 
+            for(int i = N; i>= 1; i--)
+            {
+                int pos = order[i - 1];
+                result.Insert(pos, i);
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
