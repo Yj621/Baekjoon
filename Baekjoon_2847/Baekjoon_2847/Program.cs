0808df56fb93c39c4d42b1cb40865f6d85cd25c1
Yj621 <wjddbswl26@gmail.com>
2026-02-23 22:45:32 +0900

    2847

---
+++ Baekjoon_2847/Baekjoon_2847/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Baekjoon_2847
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] level = new int[N];
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                level[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = N-2; i >=0 ; i--)
+            {
+                if (level[i] >= level[i + 1])
+                {
+                    int value = level[i + 1] - 1;
+                    count += (level[i] - value);
+                    level[i] = value;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
