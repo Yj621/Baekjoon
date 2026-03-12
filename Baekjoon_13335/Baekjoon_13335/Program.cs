f6f3d9fccc7ecc62a8b0676a14982182fb9e8b83
Yj621 <wjddbswl26@gmail.com>
2026-03-12 15:00:23 +0900

    13335

---
+++ Baekjoon_13335/Baekjoon_13335/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Baekjoon_13335
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int n = int.Parse(input[0]);
+            int w = int.Parse(input[1]);
+            int l = int.Parse(input[2]);
+
+            int time = 0;
+            int currentWeight = 0;
+
+            Queue<int> truck = new Queue<int>();
+            Queue<int> bridge = new Queue<int>();
+            string[] truckWeight = Console.ReadLine().Split();
+            for (int i = 0; i < n; i++)
+            {
+                truck.Enqueue(int.Parse(truckWeight[i]));
+            }
+
+            for (int i = 0; i < w; i++)
+            {
+                bridge.Enqueue(0);
+            }
+            while (truck.Count > 0)
+            {
+                currentWeight -= bridge.Dequeue();
+
+                if (truck.Peek() + currentWeight <= l)
+                {
+                    int nextTruck = truck.Dequeue(); // 트럭 한 대를 꺼내서 저장
+                    bridge.Enqueue(nextTruck);       // 그 트럭을 다리에 올림
+                    currentWeight += nextTruck;      // 현재 다리 무게에 더함
+                }
+                else
+                {
+                    bridge.Enqueue(0);
+                }
+                time++;
+            }
+            time += w;
+            Console.WriteLine(time);
+        }
+    }
+}
